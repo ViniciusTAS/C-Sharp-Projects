02bebd32a47f921d5762fb8d28a133cf7611a3fd
Vinicius Alves de Santana <55450201+ViniciusTAS@users.noreply.github.com>
2020-02-05 23:13:09 -0300

    Add files via upload

---
+++ Estoque/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:\n");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p + "\n");
+         
+                Console.Write("\nDigite o número de produtos a ser adicionado ao estoque: ");
+                int qte = int.Parse(Console.ReadLine());
+                p.AdicionarProdutos(qte);
+
+                Console.WriteLine("\nDados atualizados: " + p + "\n");
+
+                Console.Write("\nDigite o número de produtos a ser removido do estoque: ");
+                qte = int.Parse(Console.ReadLine());
+                p.RemoverProdutos(qte);
+                Console.WriteLine("\nDados atualizados: " + p);
+            
+        }
+    }
+}
